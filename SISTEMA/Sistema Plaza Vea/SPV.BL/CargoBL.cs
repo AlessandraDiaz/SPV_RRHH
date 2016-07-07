ad6c94e6c988c757e4fc4e72fd2e11e6f31dff1e
AlessandraDiaz <aless.diaz1@gmail.com>
2016-07-07 04:59:27 -0500

    Version 1.3.1

---
+++ SISTEMA/Sistema Plaza Vea/SPV.BL/CargoBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SPV.DA;
+using SPV.BE;
+
+namespace SPV.BL
+{
+    public class CargoBL
+    {
+        public List<CargoBE>ListaCargo(CargoBE cargo)
+        {
+            return new CargoDA().ListarCargo(cargo);
+        }
+
+    }
+}
